6e00ddff7dcac128663d5487aeb82a6140eecfdd
Volodymyr Minchenko <voldemarprk2001@gmail.com>
2022-10-06 14:10:09 +0300

    feat: + logger-app pino logging

--- logger-app/src/app.ts
+++ logger-app/src/app.ts
@@ -1,12 +1,14 @@
-/* eslint-disable no-console */
 import { AmqpQueue } from './common/enums/enums.js';
+import { logger } from './services/logger/logger.js';
 import { initServices } from './services/services.js';
 
 (async (): Promise<void> => {
   const { amqp } = await initServices();
 
+  logger.info('Connect to RabbitMQ success!');
+
   amqp.consume({
     queue: AmqpQueue.ERROR_LOGS,
-    onMessage: (data: Buffer) => console.log('data', JSON.parse(data.toString('utf-8'))),
+    onMessage: (data: Buffer) => logger.error(JSON.parse(data.toString('utf-8'))),
   });
 })();
